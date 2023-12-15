bcff57b1971860f5b5a734febe50bbb15cc1587e
devq <devq78@gmail.com>
2023-12-15 14:30:19 +0100

    init app

---
+++ src/main/java/net/devquest/App.java
@@ -0,0 +1,10 @@
+package net.devquest;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("hello bi-report");
+	}
+	public String SamMethod() {
+		return "hello bi-report";
+	}
+}
---
+++ src/test/java/net/devquest/SamMethodTest.java
@@ -0,0 +1,14 @@
+package net.devquest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SamMethodTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
