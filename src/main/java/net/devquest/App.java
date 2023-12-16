76280641b8de19524521787a5d5408893dfd3971
devq <devq78@gmail.com>
2023-12-16 02:18:05 +0100

    init

--- src/main/java/net/devquest/App.java
+++ src/main/java/net/devquest/App.java
@@ -3,6 +3,7 @@ package net.devquest;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("hello bi-report");
+		System.out.println("hello bi-report");
 	}
 	public String SamMethod() {
 		return "hello bi-report";
